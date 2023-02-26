9d9b84b4ad4c265f052b723da26b2ff3a452855a
aniueqand <88556513+aniueqand@users.noreply.github.com>
2023-02-26 16:25:10 +0300

    Update lol.py

--- Instagram Profile Picture/lol.py
+++ Instagram Profile Picture/lol.py
@@ -1,3 +1,4 @@
+
 import json
 import requests
 import hashlib
@@ -7,7 +8,7 @@ import random
 import string
 
 # Replace {username} with the Instagram username of the profile picture you want to monitor
-username = "username"
+username = "ire3mllyy"
 
 # Define the file path to save the profile picture
 file_path = "profile.jpg"
@@ -16,8 +17,10 @@ file_path = "profile.jpg"
 prev_hash = ""
 
 while True:
+ try:
+
     # Make a GET request to the Instagram API to get user information
-    response = requests.get(f"https://www.instagram.com/username/?__a=1&__d=dis")
+    response = requests.get(f"https://www.instagram.com/ire3mllyy/?__a=1&__d=dis")
     
     # Extract the profile picture URL from the JSON response
     data = json.loads(response.text)
@@ -41,10 +44,13 @@ while True:
         print("Profile picture updated.")
         if os.path.isfile('D:\ire3mllyy\profile.jpg'):
               random_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=100))
-              os.rename("D:\username\profile.jpg", "D:\username\ "+ random_name + ".jpg")
-
-    else:
+              os.rename("D:\ire3mllyy\profile.jpg", "D:\ire3mllyy\ "+ random_name + ".jpg")
+ except Exception as ni:
+    print(ni)
+    time.sleep(120)
+else:
         print("Profile picture not updated.")
     
-    # Wait for 60 seconds before checking again
-    time.sleep(60)
+    # Wait for 10 seconds before checking again
+time.sleep(120)
+
