3537f94320c0fc589a828629bff904a8aaf6baea
aniueqand <88556513+aniueqand@users.noreply.github.com>
2023-02-28 20:58:57 +0300

    Update lol.py

--- Instagram Profile Picture/lol.py
+++ Instagram Profile Picture/lol.py
@@ -7,26 +7,20 @@ import os
 import random
 import string
 
-# Replace {username} with the Instagram username of the profile picture you want to monitor
-username = "ire3mllyy"
-
-# Define the file path to save the profile picture
+# Replace {username} with the Instagram username of the profile picture you want
+username = "type username"
 file_path = "profile.jpg"
-
-# Initialize the previous profile picture hash value
 prev_hash = ""
 
 while True:
  try:
 
     # Make a GET request to the Instagram API to get user information
-    response = requests.get(f"https://www.instagram.com/ire3mllyy/?__a=1&__d=dis")
+    response = requests.get(f"https://www.instagram.com/{username}/?__a=1&__d=dis")
     
     # Extract the profile picture URL from the JSON response
     data = json.loads(response.text)
     profile_pic_url = data['graphql']['user']['profile_pic_url_hd']
-    
-    # Compute the hash value of the profile picture URL
     current_hash = hashlib.sha256(profile_pic_url.encode()).hexdigest()
     
     # Compare the current hash value with the previous hash value
@@ -42,15 +36,15 @@ while True:
         prev_hash = current_hash
         
         print("Profile picture updated.")
-        if os.path.isfile('D:\ire3mllyy\profile.jpg'):
+        if os.path.isfile('D:\username\profile.jpg'):
               random_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=100))
-              os.rename("D:\ire3mllyy\profile.jpg", "D:\ire3mllyy\ "+ random_name + ".jpg")
+              os.rename("D:\username\profile.jpg", "D:\username\ "+ random_name + ".jpg")
  except Exception as ni:
     print(ni)
     time.sleep(120)
 else:
         print("Profile picture not updated.")
     
-    # Wait for 10 seconds before checking again
+    # Wait for 120 seconds before checking again
 time.sleep(120)
 
